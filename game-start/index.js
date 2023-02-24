cd76093e82abe63e0427b8ce60db98b48b2c4c63
Thomas van Steen <90308865+ThomasvanSteen@users.noreply.github.com>
2023-02-24 12:23:29 +0100

    final update for now

--- game-start/index.js
+++ game-start/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
